2d4cf44b46bd5b45859595b98cb2c993cb56432e
Vadim Kovalev <nikas.367@mail.ru>
2024-02-20 15:04:05 +0300

    modifed

--- js/helper.js
+++ js/helper.js
@@ -1,1 +1,23 @@
+let time = document.querySelector('.time');
+let date = document.querySelector('.date');
+let arrowHours = document.querySelector('#arrow__hours');
+let arrowMinutes = document.querySelector('#arrow__minutes');
+
+function rotate(deg) {
+  deg *= 0.1
+  return deg
+}
+
 export const aormatedTime = (numb) => +numb < 10 ? '0' + +numb : numb
+
+export function newTimeDate() {
+  let newTime = new Date();
+  time.innerHTML = (aormatedTime(newTime.getHours()) + ':' + aormatedTime(newTime.getMinutes()) + ':' + aormatedTime(newTime.getSeconds()));
+  date.innerHTML = (newTime.getDate() + " " + newTime.toLocaleString('en', { month: 'long' }) + ', ' + newTime.getFullYear());
+}
+
+export function newClock(hours, minutes) {
+  arrowHours.style.cssText = `transform: rotate(${rotate(hours)}deg)`
+  arrowMinutes.style.cssText = `transform: rotate(${rotate(minutes)}deg)`
+}
+
--- js/index.js
+++ js/index.js
@@ -1,19 +1,15 @@
-let time = document.querySelector('.time');
-let date = document.querySelector('.date');
-let arrowSeconds = document.querySelector('#arrow__seconds')
-import {aormatedTime} from './helper.js';
+import { newTimeDate, newClock } from './helper.js';
 
+newTimeDate();
+newClock();
 
 setInterval(function() {
   let newTime = new Date();
-  let seconds = newTime.getSeconds();
-  console.log(seconds)
+  let minutes = newTime.getMinutes() * 60;
+  let hours = newTime.getHours() * 60 + minutes / 12;
 
-  time.innerHTML = (aormatedTime(newTime.getHours()) + ':' + aormatedTime(newTime.getMinutes()) + ':' + aormatedTime(newTime.getSeconds()));
-  date.innerHTML = (newTime.getDate() + " " + newTime.toLocaleString('en', { month: 'long' }) + ', ' + newTime.getFullYear());
-
-  // arrowSeconds.style.cssText = `transofrm: rotate${seconds}deg`
+  newTimeDate();
+  newClock(hours, minutes);
 }, 1000);
-
 
 
